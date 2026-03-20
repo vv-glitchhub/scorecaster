c05457a1d6ae2b65bd572ae04a75dd4603a6ab5a
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-03-20 19:57:24 +0200

    Add page component with styled SCORECASTER text
    Created a new page component with a styled div.

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 30,
+        fontFamily: "Arial, sans-serif"
+      }}
+    >
+      SCORECASTER
+    </div>
+  );
+}
