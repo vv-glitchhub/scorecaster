eb87bb4112ee93e3bd73c26db95f48722b9b41f7
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-03-21 06:01:10 +0200

    Add styles to body in RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="fi">
-      <body>{children}</body>
+      <body style={{ margin: 0, background: "#07070f" }}>
+        {children}
+      </body>
     </html>
   );
 }
