01ab699f84ae6d50056689a7ff68b534991a9e55
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-03-28 16:09:52 +0200

    Create route.js

---
+++ app/api/Odds/route.js
@@ -0,0 +1,16 @@
+export async function GET(req) {
+  const { searchParams } = new URL(req.url)
+  const sport = searchParams.get("sport") || "icehockey_nhl"
+
+  try {
+    const res = await fetch(
+      `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${process.env.ODDS_API_KEY}&regions=eu&markets=h2h`
+    )
+
+    const data = await res.json()
+
+    return Response.json(data)
+  } catch (error) {
+    return Response.json({ error: "Failed to fetch odds" })
+  }
+}
