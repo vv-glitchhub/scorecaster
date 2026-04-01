d1528496fd048f7c662a506c71158822472a400a
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-04-01 12:14:14 +0300

    Update route.js

--- app/api/simulator/route.js
+++ app/api/simulator/route.js
@@ -1,10 +1,19 @@
-import { DEFAULT_TEAM_DATA } from "../../../lib/team-ratings";
+import {
+  DEFAULT_TEAM_DATA,
+  RATING_WEIGHTS,
+  getAllTeamRatings,
+} from "../../../lib/team-ratings";
 import { simulateWorldChampionshipManyTimes } from "../../../lib/simulator";
 
 export async function GET() {
   try {
     const iterations = 5000;
-    const results = simulateWorldChampionshipManyTimes(iterations, DEFAULT_TEAM_DATA);
+    const results = simulateWorldChampionshipManyTimes(
+      iterations,
+      DEFAULT_TEAM_DATA
+    );
+
+    const teamRatings = getAllTeamRatings(DEFAULT_TEAM_DATA);
 
     return Response.json({
       ok: true,
@@ -25,7 +34,14 @@ export async function GET() {
           "markkinafutures-kertoimet",
           "valmennuksen taktiset erot",
         ],
+        ratingFormula: {
+          attackWeight: RATING_WEIGHTS.attack,
+          defenseWeight: RATING_WEIGHTS.defense,
+          goalieWeight: RATING_WEIGHTS.goalie,
+          formWeight: RATING_WEIGHTS.form,
+        },
       },
+      teamRatings,
       results,
     });
   } catch (error) {
@@ -34,6 +50,7 @@ export async function GET() {
     return Response.json({
       ok: false,
       error: String(error),
+      teamRatings: [],
       results: [],
     });
   }
