a3f5ff2f1a5de7a502164b4c914d03d1ec9e6613
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-07-14 08:45:32 +0300

    Add cloud sync page

---
+++ app/cloud-sync/page.jsx
@@ -0,0 +1,9 @@
+import CloudSyncClient from "./cloud-sync-client";
+
+export const metadata = {
+  title: "Cloud Sync | Scorecaster"
+};
+
+export default function CloudSyncPage() {
+  return <CloudSyncClient />;
+}
