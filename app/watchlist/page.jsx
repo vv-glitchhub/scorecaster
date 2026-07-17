fe41b9772c7a1c5d4c3cad29bd24cc5c9c20f557
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-07-17 14:07:43 +0300

    Add Watchlist V2 page

---
+++ app/watchlist/page.jsx
@@ -0,0 +1,9 @@
+import WatchlistClient from "./WatchlistClient";
+
+export const metadata = {
+  title: "Watchlist & Alerts"
+};
+
+export default function WatchlistPage() {
+  return <WatchlistClient />;
+}
