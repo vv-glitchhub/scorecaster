72709940ec22b1c7a385bf2485d1a237b104c0c9
vv-glitchhub <vikke.vuorio99@gmail.com>
2026-09-04 08:01:03 +0300

    Integrate external slips into My Picks

--- app/tracking/layout.jsx
+++ app/tracking/layout.jsx
@@ -1,10 +1,12 @@
 import { ProfessionalPortfolioRail } from "../components/ProfessionalSurfaceRail";
+import ExternalSlipTrackerConnected from "./ExternalSlipTrackerConnected";
 
 export default function TrackingLayout({ children }) {
   return (
     <div className="space-y-8">
       <ProfessionalPortfolioRail />
       {children}
+      <ExternalSlipTrackerConnected />
     </div>
   );
 }
